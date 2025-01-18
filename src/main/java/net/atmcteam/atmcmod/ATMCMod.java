60b99e3a4972c47ee7086f19cc0121fb44058127
SpionJam <spionjam@gmail.com>
2025-01-18 23:38:49 +0100

    Initial Commit

---
+++ src/main/java/net/atmcteam/atmcmod/ATMCMod.java
@@ -0,0 +1,17 @@
+package net.atmcteam.atmcmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ATMCMod implements ModInitializer {
+	public static final String MOD_ID = "atmcmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/atmcteam/atmcmod/ATMCModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.atmcteam.atmcmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ATMCModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
